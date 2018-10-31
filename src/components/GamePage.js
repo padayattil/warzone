41bba61060f367552fbff81c1c0907cf8bff0576
Antony Padayattil <antonywired@gmail.com>
2018-10-31 12:49:05 +0000

    home page

---
+++ src/components/GamePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function GamePage(props) {
+  return (
+    <header className="App-header">
+      <h3 className="m-4">
+        <strong>
+          Game
+        </strong>
+      </h3>
+    </header>
+  );
+}
+
+export default GamePage;
